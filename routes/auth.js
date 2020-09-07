4c17b3b72993bd78bbf1759b3fd32836bcba4d3b
Ujoy7851 <joy7851@gmail.com>
2020-09-07 12:24:56 +0000

    Update: 회원가입 폼 유효성 검사 추가

--- routes/auth.js
+++ routes/auth.js
@@ -8,10 +8,18 @@ const logger = require('../config/logger');
 
 router.post('/join', async (req, res, next) => {
   const { email, username, password } = req.body;
+  const emailRegExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const pwRegExp = /^[a-zA-Z0-9]{6,12}$/
   try {
     const user = await User.findOne({ where: { email } });
     if(user) {
       req.flash('joinError', '이미 가입되어 있습니다.');
+      return res.redirect('/join');
+    } else if(!emailRegExp.test(email)) {
+      req.flash('joinError', '이메일 형식이 옳지 않습니다.');
+      return res.redirect('/join');
+    } else if(!pwRegExp.test(password)) {
+      req.flash('joinError', '비밀번호는 영문 대소문자와 숫자 6~12자리로 입력하세요.');
       return res.redirect('/join');
     } else {
       const hash = await bcrypt.hash(password, 12);
